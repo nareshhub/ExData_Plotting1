c4807d1979c603ad7d55c63ad599d6d2e0ff9613
nareshhub <naresh_polo@yahoo.co.in>
2015-10-11 03:08:40 -0500

    ExData-V1.1

--- plot1.R
+++ plot1.R
@@ -3,6 +3,8 @@ names <- names(powerin)
 datain <- read.table(pipe("findstr /B /R ^[1-2]/2/2007 household_power_consumption.txt"),header=T,sep=";",stringsAsFactors = F)
 colnames(datain) <- names
 
+png("plot1.png",height=480,width=480)
 par(mfrow=c(1,1))
 hist(datain$Global_active_power,xlab="Global Active Power (kilowatts)",ylab="Frequency",
         main="Global Active Power",col="red")
+dev.off()
--- plot2.R
+++ plot2.R
@@ -5,8 +5,7 @@ colnames(datain) <- names
 datain$dateTime <- paste(datain$Date,datain$Time,sep=" ")
 datain$dateTime <- strptime(datain$dateTime,"%d/%m/%Y %H:%M:%S")
 
+png("plot2.png",height=480,width=480)
 par(mfrow=c(1,1))
-plot(datain$dateTime,datain$Sub_metering_1,xlab = "",ylab="Energy Sub Metering",type = "l",col="black")
-lines(datain$dateTime,datain$Sub_metering_2,type="l",col="red")
-lines(datain$dateTime,datain$Sub_metering_3,type="l",col="blue")
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","green"),x.intersp=0.2, y.intersp=0.3,cex=0.7)
+plot(datain$dateTime,datain$Global_active_power,xlab = "",ylab="Global Active Power (kilowatts)",type = "l")
+dev.off()
